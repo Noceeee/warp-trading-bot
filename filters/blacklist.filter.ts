ed98ec88902c16de9f9be60777e45efde4496c6e
george_81 <109899587+Noceeee@users.noreply.github.com>
2024-05-18 01:27:53 +0200

    Update blacklist.filter.ts
    Signed-off-by: george_81 <109899587+Noceeee@users.noreply.github.com>

--- filters/blacklist.filter.ts
+++ filters/blacklist.filter.ts
@@ -1,49 +1,47 @@
 import { Filter, FilterResult } from './pool-filters';
-import { Connection, PublicKey } from '@solana/web3.js';
+import { Connection } from '@solana/web3.js';
 import { LiquidityPoolKeysV4 } from '@raydium-io/raydium-sdk';
 import { getPdaMetadataKey } from '@raydium-io/raydium-sdk';
 import { MetadataAccountData, MetadataAccountDataArgs } from '@metaplex-foundation/mpl-token-metadata';
 import { Serializer } from '@metaplex-foundation/umi/serializers';
 import { logger } from '../helpers';
+import { readFileSync } from 'fs';
 
 export class BlacklistFilter implements Filter {
+    private readonly blacklist: string;
 
-  constructor(
-    private readonly connection: Connection,
-    private readonly metadataSerializer: Serializer<MetadataAccountDataArgs, MetadataAccountData>
-  ) { }
-
-  async execute(poolKeys: LiquidityPoolKeysV4): Promise<FilterResult> {
-    try {
-
-      let blacklist = [
-        "8N57pYk1SoHkBZcPMhyJMJCLLfEyHMq3SqYn63W9RoVG",
-        "GH8GPjSX9XNvxsVaJHg9KfEXVovqtiY5pyhu8vYrwjTb",
-        "BpHihYFRjTywHg6J2KmL8yWGsAQB9Ab2W3bGQyLBzbek"
-      ];
-
-      const metadataPDA = getPdaMetadataKey(poolKeys.baseMint);
-      const metadataAccount = await this.connection.getAccountInfo(metadataPDA.publicKey, this.connection.commitment);
-
-      if (!metadataAccount?.data) {
-        return { ok: false, message: 'Blacklist -> Failed to fetch account data' };
-      }
-
-      const deserialize = this.metadataSerializer.deserialize(metadataAccount.data);
-
-      if (blacklist.includes(deserialize[0].updateAuthority.toString())) {
-        return { ok: false, message: `Blacklist -> ${deserialize[0].updateAuthority.toString()} fuck this guy!` };
-      }
-
-      return { ok: true, message: undefined };
-
-    } catch (e) {
-      logger.error({ mint: poolKeys.baseMint }, `Blacklist -> Failed to check blacklist`);
+    constructor(
+        private readonly connection: Connection,
+        private readonly metadataSerializer: Serializer<MetadataAccountDataArgs, MetadataAccountData>
+    ) {
+        // Lee el contenido de la lista negra en el constructor
+        this.blacklist = readFileSync('./blacklist.txt', 'utf-8');
     }
 
-    return {
-      ok: false,
-      message: `Blacklist -> Failed to check for cringe`,
-    };
-  }
+    async execute(poolKeys: LiquidityPoolKeysV4): Promise<FilterResult> {
+        try {
+            const metadataPDA = getPdaMetadataKey(poolKeys.baseMint);
+            const metadataAccount = await this.connection.getAccountInfo(metadataPDA.publicKey, this.connection.commitment);
+
+            if (!metadataAccount?.data) {
+                return { ok: false, message: 'Blacklist -> Failed to fetch account data' };
+            }
+
+            const deserialize = this.metadataSerializer.deserialize(metadataAccount.data);
+
+            if (this.blacklist.includes(deserialize[0].updateAuthority.toString())) {
+                return { ok: false, message: `Blacklist -> ${deserialize[0].updateAuthority.toString()} fuck this guy!` };
+            }
+
+            return { ok: true, message: undefined };
+
+        } catch (e) {
+            logger.error({ mint: poolKeys.baseMint }, `Blacklist -> Failed to check blacklist`);
+        }
+
+        return {
+            ok: false,
+            message: `Blacklist -> Failed to check for cringe`,
+        };
+    }
 }
