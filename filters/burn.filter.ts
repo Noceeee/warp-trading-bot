54bab13cb3f57cd384112188904f903a47856d22
george_81 <109899587+Noceeee@users.noreply.github.com>
2024-06-11 11:02:42 +0200

    Update burn.filter.ts
    Signed-off-by: george_81 <109899587+Noceeee@users.noreply.github.com>

--- filters/burn.filter.ts
+++ filters/burn.filter.ts
@@ -4,15 +4,31 @@ import { LiquidityPoolKeysV4 } from '@raydium-io/raydium-sdk';
 import { logger } from '../helpers';
 
 export class BurnFilter implements Filter {
+  private cachedResult: FilterResult | undefined = undefined;
+
   constructor(private readonly connection: Connection) {}
 
   async execute(poolKeys: LiquidityPoolKeysV4): Promise<FilterResult> {
+    if (this.cachedResult) {
+      return this.cachedResult;
+    }
+
     try {
       const amount = await this.connection.getTokenSupply(poolKeys.lpMint, this.connection.commitment);
       const burned = amount.value.uiAmount === 0;
-      return { ok: burned, message: burned ? undefined : "Burned -> Creator didn't burn LP" };
+      const result = { ok: burned, message: burned ? undefined : "Burned -> Creator didn't burn LP" };
+
+      if (result.ok) {
+        this.cachedResult = result;
+      }
+
+      return result;
     } catch (e: any) {
-      logger.error({ mint: poolKeys.baseMint, error: e }, `Burned -> Failed to check if LP is burned`);
+      if (e.code == -32602) {
+        return { ok: true };
+      }
+
+      logger.error({ mint: poolKeys.baseMint }, `Failed to check if LP is burned`);
     }
 
     return { ok: false, message: 'Failed to check if LP is burned' };
